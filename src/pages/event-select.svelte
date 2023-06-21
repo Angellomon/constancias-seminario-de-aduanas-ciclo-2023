be297488d4ad838e67462de2560e8e03523c70a3
Angellomon <angelmtzdiaz@gmail.com>
2023-06-21 17:07:18 -0600

    feat(pages): impl Event Select Page

--- src/pages/event-select.svelte
+++ src/pages/event-select.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { EventSelector } from "../lib/components/content";
+  import MainFooter from "../lib/components/footer/main-footer.svelte";
   import { HeaderEventSelect } from "../lib/components/headers";
 </script>
 
 <main>
   <HeaderEventSelect />
+  <EventSelector />
+  <MainFooter />
 </main>
