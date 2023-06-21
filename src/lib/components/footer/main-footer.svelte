988fabb5f1471dc165b21b3ad1c5169255df6a4f
Angellomon <angelmtzdiaz@gmail.com>
2023-06-21 17:06:56 -0600

    feat(components): impl footer

---
+++ src/lib/components/footer/main-footer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { logosSources } from "../../store";
+</script>
+
+<footer>
+  {#each $logosSources as logoSrc}
+    <img src={logoSrc.file} alt={logoSrc.name.toUpperCase()} />
+  {/each}
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+
+    background-color: white;
+  }
+
+  img {
+    height: 15vh;
+  }
+</style>
