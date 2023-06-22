cd274f19052c4470628a271839298a40fa0f5f08
Angellomon <angelmtzdiaz@gmail.com>
2023-06-22 00:09:16 -0600

    feat(components/img): impl chevron component

---
+++ src/lib/components/img/chevron-right.svelte
@@ -0,0 +1,13 @@
+<img src="/svg/chevron-right-solid.svg" alt=">" />
+
+<style>
+  img {
+    height: 1.4rem;
+    width: 1.4rem;
+
+    filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(55deg)
+      brightness(103%) contrast(103%);
+
+    cursor: pointer;
+  }
+</style>
