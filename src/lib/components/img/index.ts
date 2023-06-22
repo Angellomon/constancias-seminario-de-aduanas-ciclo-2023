09ecbe1c104efabfc2124ebe1d1d2d99d4fcaab1
Angellomon <angelmtzdiaz@gmail.com>
2023-06-22 17:20:17 -0600

    feat(components/img): impl spinner components

--- src/lib/components/img/index.ts
+++ src/lib/components/img/index.ts
@@ -1,2 +1,3 @@
 export { default as MainLogoTransparent } from "./main-logo-transparent.svelte";
 export { default as ChevronRight } from "./chevron-right.svelte";
+export { default as Spinner } from "./spinner.svelte";
