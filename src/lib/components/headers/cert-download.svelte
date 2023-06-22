7cfa37b86b0e3fb5d84ed28b4d1815fdb39c4fb4
Angellomon <angelmtzdiaz@gmail.com>
2023-06-21 22:59:02 -0600

    feat(components/headers): impl cert download header component

---
+++ src/lib/components/headers/cert-download.svelte
@@ -0,0 +1,72 @@
+<script>
+  import { MainLogoTransparent } from "../img";
+</script>
+
+<header>
+  <div class="wrapper">
+    <MainLogoTransparent absolute />
+
+    <div class="title">
+      <h1>CONSTANCIAS</h1>
+    </div>
+
+    <div class="border" />
+  </div>
+</header>
+
+<style>
+  header {
+    height: 50vh;
+
+    padding: 15px;
+    padding-bottom: 20px;
+
+    position: relative;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  div.border {
+    position: absolute;
+    top: 5;
+    left: 5;
+    right: 5;
+    bottom: 5;
+    z-index: 0;
+
+    width: 95%;
+    height: 85%;
+
+    border: 4px solid white;
+    border-radius: 20px;
+  }
+
+  div.title {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+
+    /* margin-top: -5vh; */
+    width: 100%;
+    height: 20vh;
+
+    background-color: var(--main-green-solid);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div.wrapper {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-flow: column;
+    justify-content: flex-end;
+    align-items: center;
+  }
+</style>
--- src/lib/components/headers/index.ts
+++ src/lib/components/headers/index.ts
@@ -1,1 +1,2 @@
 export { default as HeaderEventSelect } from "./event-select.svelte";
+export { default as HeaderCertDownload } from "./cert-download.svelte";
