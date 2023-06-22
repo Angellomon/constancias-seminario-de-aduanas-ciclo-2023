7b6fb797cbdf0206e909e83d9ca2f4f49fc542d3
Angellomon <angelmtzdiaz@gmail.com>
2023-06-22 00:09:54 -0600

    feat(components/content): impl CertDownload component

---
+++ src/lib/components/content/cert-download.svelte
@@ -0,0 +1,74 @@
+<script>
+  import { ChevronRight } from "../img";
+</script>
+
+<section>
+  <h2>Correo Electrónico</h2>
+
+  <label for="email">
+    <input name="email" type="email" />
+    <ChevronRight />
+  </label>
+
+  <div class="message">
+    <p class="first">
+      Ingresa el correo electrónico con el cual te registraste
+    </p>
+    <p class="second">al Seminario Interactivo de Aduanas 2023</p>
+  </div>
+</section>
+
+<style>
+  section {
+    height: 60vh;
+
+    padding-top: 2vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  div.message {
+    margin-top: 5vh;
+    /* margin-bottom: 5vh; */
+    /* text-justify: inter-character; */
+    text-align: justify;
+    vertical-align: middle;
+
+    width: 60vw;
+  }
+
+  p {
+    margin: 0;
+    height: 1.2rem;
+
+    /* width: 100%; */
+  }
+
+  p::after {
+    content: "";
+    display: inline-block;
+    width: 100%;
+  }
+
+  p.second {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    p {
+      height: 2.2rem;
+    }
+
+    div.message {
+      width: 70vw;
+    }
+  }
+  @media screen and (max-width: 375px) {
+    p {
+      height: 3rem;
+    }
+  }
+</style>
--- src/lib/components/content/index.ts
+++ src/lib/components/content/index.ts
@@ -1,1 +1,2 @@
 export { default as EventSelector } from "./event-selector.svelte";
+export { default as CertDownloadContent } from "./cert-download.svelte";
